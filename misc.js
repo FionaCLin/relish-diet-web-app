8c1bd96c89f250c0f05636625b0fd430a215db1f
ines219 <ines.sarmiento1@gmail.com>
2018-05-03 21:20:39 +1000

    progress commit, starting on bookmarked, and the my pages

--- misc.js
+++ misc.js
@@ -25,3 +25,19 @@ function includeHTML() {
     }
   }
 }
+
+function SwapDivsWithClick(div1,div2)
+{
+   d1 = document.getElementById(div1);
+   d2 = document.getElementById(div2);
+   if( d2.style.display == "none" )
+   {
+      d1.style.display = "none";
+      d2.style.display = "block";
+   }
+   else
+   {
+      d1.style.display = "block";
+      d2.style.display = "none";
+   }
+}
