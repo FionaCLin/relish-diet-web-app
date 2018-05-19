55df51955a2975d4377495dcf729ac28d45a07ca
FionaCLin <clin4614@uni.sydney.edu.au>
2018-05-20 09:31:03 +1000

    Update UI app

--- servers/fresh-fridge/src/index.js
+++ servers/fresh-fridge/src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import App from './components/App';
+import reducer from './reducers';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const store = createStore(reducer)
+
+
+ReactDOM.render(<Provider store={store}>
+  <App />
+</Provider>, document.getElementById('root'));
 registerServiceWorker();
