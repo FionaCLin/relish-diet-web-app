addd1cfb3c59749f0ad61ee2c87ba0a73181a087
Fiona <fiona@zentri.com>
2018-05-21 14:01:22 +1000

    return response token

--- servers/fresh-fridge/src/api.js
+++ servers/fresh-fridge/src/api.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 function login(email, password) {
   console.log(email, password)
+  let token = null;
   axios.post('http://localhost:3002/api/users/login', {
     // email: 'synexenel1416@yopmail.com',
     // password: '123'
@@ -9,11 +10,13 @@ function login(email, password) {
     password: password
   }).then(function (response) {
     console.log('you have login');
+    //suppose the response contain the token?
+    token = response.data.token;
   }).catch(function (error) {
     console.log('you haven\'t login');
     console.log(error);
   });
-
+  return token;
 }
 
 export default {
