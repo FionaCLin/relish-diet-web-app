186939632618e05bd5ab8b64f53e165169903985
ines219 <ines.sarmiento1@gmail.com>
2018-05-23 02:52:47 +1000

    edit

--- servers/fresh-fridge/src/components/MealPlanner.js
+++ servers/fresh-fridge/src/components/MealPlanner.js
@@ -11,10 +11,10 @@ const recipeInfo = [
         name: 'Popeyetoast with eggs',
         img: 'images/recipe.jpg',
         macros: {
-            Intake: 10,
-            Carbs: 20,
-            Protein: 30,
-            Fats: 40,
+            Intake: 1000,
+            Carbs: 200,
+            Protein: 300,
+            Fats: 400,
             Sodium: 50
         }
     },
@@ -74,6 +74,7 @@ class MealPlanner extends React.Component {
         this.dragOut = true;
         this.dragOutNum = null;
         this.state = {
+            showMacro: null,
             name: "Bodybuilding plan (PART A)",
             dailyMeals : [
                 [null, null, null],
@@ -130,6 +131,10 @@ class MealPlanner extends React.Component {
         return nutrientValue;
     }
 
+    getMeasurement = (nutrient) => {
+        return ((nutrient == 'Intake') ? 'kj' : 'g');
+    }
+
     cancel = () => {
         console.log("Cancelled");
     }
@@ -138,8 +143,24 @@ class MealPlanner extends React.Component {
         console.log("Finish Edit");
     }
 
+    macroOver = (e, id) => {
+        e.preventDefault();
+        let { showMacro } = this.state;
+        showMacro = id;
+        this.setState({showMacro});
+        console.log('enter');
+    }
+
+    macroOut = (e) => {
+        e.preventDefault();
+        let { showMacro } = this.state;
+        showMacro = null;
+        this.setState({ showMacro });
+        console.log('out');
+    }
+
     render() {
-        const { name, dailyMeals } = this.state;
+        const { showMacro, name, dailyMeals } = this.state;
 
         return (
             <div class="body_container">
@@ -169,7 +190,13 @@ class MealPlanner extends React.Component {
                                                         draggable="true" onDragStart={ (e) => this.onDragStart(e, day[mealKey])}
                                                         onDragEnd={ (e) => this.onDragEnd(e, [dayKey, mealKey])}
                                                         onDragOver={this.allowDrop} onDrop={(e) => this.onDrop(e, [dayKey, mealKey])}>
+                                                        <div class="drop_img_wrapper" centred>
+                                                            <div class="overlay">
+                                                                <div class="planner_img_text">{this.getRecipe(day[mealKey]).name}</div>
+                                                            </div>
+                                                        </div>
                                                     </td>
+                                                    
                                         } else {
                                             return <td class="planner_img"  onDragOver={this.allowDrop} onDrop={(e) => this.onDrop(e, [dayKey, mealKey])}></td>
                                         }
@@ -183,11 +210,10 @@ class MealPlanner extends React.Component {
                             {
                                 constants.mealPlanner.macroNutrients.map((nutrient) => {
                                     let macroHead = (nutrient == 'Intake') ? <th scope="row" rowspan="5"><div class="vertical macroDiv">MACROS</div></th> : null;
-                                    let measure = (nutrient == 'Intake') ? 'kj' : 'g';
                                     let timeSlots = dailyMeals.map(day => {
                                         return <td class="macro_img">
                                                     <div class="macroLeft">{ nutrient }</div>
-                                                    <div class="macroRight">{ this.calculateNutrient(day, nutrient) } { measure }</div>
+                                                    <div class="macroRight">{ this.calculateNutrient(day, nutrient) } { this.getMeasurement(nutrient) }</div>
                                                 </td>
                                     })
                                     return <tr>{ macroHead }{ timeSlots }</tr>
@@ -197,25 +223,41 @@ class MealPlanner extends React.Component {
                     </table>
                 </div>
         
-                <div class="dropArea">
-                    <div class="panel panel-default">
-                        <div class="panel-heading">Recipes</div>
-                        <div class="panel-body pre-scrollable dropPanel">
-                            <div class="list-group">
-                                {
-                                    recipeInfo.map(recipe => {
-                                        return <div class="list-group-item list-group-item-action planner_img_wrapper">
-                                                    <div class="planner_img" draggable="true" style={ bg_img(recipe.img) } onDragStart={ (e) => this.onDragStart(e, recipe.id) }></div>
-                                                </div>
-                                    })
-                                }
+                <div style={{float:"right"}}>
+                    <div style={{float:"left"}} class="dropArea">
+                        <div class="panel panel-default">
+                            <div class="panel-heading">Recipes</div>
+                            <div class="panel-body pre-scrollable dropPanel">
+                                <div class="list-group">
+                                    {
+                                        recipeInfo.map(recipe => {
+                                            let macros = constants.mealPlanner.macroNutrients.map(nutrient => {
+                                                return <tr><td style={{float:'left',width:75}}>{nutrient}</td><td style={{float:'left',width:75}}>{recipe.macros[nutrient]} {this.getMeasurement(nutrient)}</td></tr>
+                                            })
+                                            return <div class="list-group-item list-group-item-action planner_img_wrapper">
+                                                        <div class="macroInfo btn-xs btn-default" onMouseEnter={ (e) => this.macroOver(e, recipe.id) } onMouseLeave={(e) => this.macroOut(e)}><span class="glyphicon glyphicon-signal" aria-hidden="true" ></span></div>
+                                                        <div class="planner_img" draggable="true" style={ bg_img(recipe.img) } onDragStart={ (e) => this.onDragStart(e, recipe.id) }></div>
+                                                        <div class="overlay">
+                                                            <div class="planner_img_text" centred>
+                                                                { (showMacro == recipe.id) ? <table>{macros}</table> : recipe.name } 
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                        })
+                                    }
+                                </div>
                             </div>
                         </div>
+                        <br></br>
+                        <div class="macroRight">
+                            <button onClick={ this.editPlanner() } class="btn btn-success editSubmit">Edit</button>
+                            <button onClick={ this.cancel() } class="btn btn-secondary editCancel">Cancel</button>
+                        </div>
                     </div>
-                    <br></br>
-                    <div class="macroRight">
-                        <button onClick={ this.editPlanner() } class="btn btn-success editSubmit">Edit</button>
-                        <button onClick={ this.cancel() } class="btn btn-secondary editCancel">Cancel</button>
+                    <div style={{float:"left", width: 20}}>
+                        <button class="btn btn-default sideButton" title="Personal Recipes"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></button>
+                        <button class="btn btn-default sideButton" title="Bookmarked Recipes"><span class="glyphicon glyphicon-bookmark" aria-hidden="true"></span></button>
+                        <button class="btn btn-default sideButton" title="Recommended Recipes"><span class="glyphicon glyphicon-thumbs-up" aria-hidden="true"></span></button>
                     </div>
                 </div>
             </div>
