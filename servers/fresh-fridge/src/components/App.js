931d54369556d5c3d54ca9e6e950bc9605fec295
FionaCLin <clin4614@uni.sydney.edu.au>
2018-05-23 05:01:47 +1000

    Add Router and routes

--- servers/fresh-fridge/src/components/App.js
+++ servers/fresh-fridge/src/components/App.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Route from 'react-router-dom/Route';
 import MealPlanner from './MealPlanner';
+import Login from './Login';
+import Bookmark from './Bookmark';
+import Dashboard from './Dashboard';
+import RecipeList from './RecipeList';
+import RecipePage from './RecipePage';
+import NavigationBar from './NavigationBar';
 
 class App extends Component {
   render() {
     return (
-      <div className="App" store={this.props.store}>
-      <MealPlanner />      
-      </div>
+      <Router>
+        <div className="App" store={this.props.store}>
+          <NavigationBar />
+          <Route path="/login" component={Login} exact stric />
+          <Route path="/" component={Login} exact stric />
+          <Route path="/dashboard" exact strict component={Dashboard} />
+          <Route path="/search" exact strict component={Dashboard} />
+          <Route path="/mealplan" exact stric component={MealPlanner} />
+          <Route path="/bookmark" exact stric component={Bookmark} />
+          <Route path="/recipes" exact stric component={RecipeList} />
+          <Route path="/recipe" component={RecipePage} />
+          <Route path="/mealplan" exact stric component={MealPlanner} />
+        </div>
+      </Router>
     );
   }
 }
