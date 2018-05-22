b5527d66a8ec962781a44bf685127b3ff9c392cc
FionaCLin <clin4614@uni.sydney.edu.au>
2018-05-23 00:51:28 +1000

    Move bg_img as a helper function under constants/

--- servers/fresh-fridge/src/components/Background.js
+++ servers/fresh-fridge/src/components/Background.js
@@ -1,22 +1,15 @@
 import React from 'react';
-import bg1 from './images/bg1.jpg';
-import bg2 from './images/bg2.jpg';
-import bg3 from './images/bg3.jpg';
+import { bg_img } from '../constants/helper.js'
 
-const bg_img = (img) => {
-  return ({
-    backgroundImage: 'url(' + img + ')'
-  })
-}
 
 const Background = (props) => {
   return (
     <div id="background-carousel">
       <div id="myCarousel" className="carousel slide myCarouselDiv" data-ride="carousel">
         <div className="carousel-inner myCarouselDiv" >
-          <div className="item active bg_login" style={ bg_img(bg1) }></div>
-          <div className="item bg_login" style={ bg_img(bg2) }></div>
-          <div className="item bg_login" style={ bg_img(bg3) }></div>
+          <div className="item active bg_login" style={bg_img('bg1')}></div>
+          <div className="item bg_login" style={bg_img('bg2')}></div>
+          <div className="item bg_login" style={bg_img('bg3')}></div>
         </div>
       </div>
     </div >
