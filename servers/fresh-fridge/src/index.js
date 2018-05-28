3666cf9d910d97e69c7ffb768d58136acd5c4784
FionaCLin <clin4614@uni.sydney.edu.au>
2018-05-28 20:53:22 +1000

    clean up reducer

--- servers/fresh-fridge/src/index.js
+++ servers/fresh-fridge/src/index.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import App from './components/App';
-import reducer from './reducers';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
-
-const store = createStore(reducer)
-
-
-ReactDOM.render(<Provider store={store}>
+ReactDOM.render(<Provider >
   <App />
 </Provider>, document.getElementById('root'));
 registerServiceWorker();
