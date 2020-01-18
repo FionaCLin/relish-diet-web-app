1560ce99409d4ea8af67f7c7f3a941bc290223a0
FionaCLin <fiona.lin@student.unsw.edu.au>
2020-01-19 10:07:33 +1100

    Add auth middleware

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,14 +2,27 @@ var express = require("express");
 var router = express.Router();
 
 module.exports = api => {
+  router.use((req, res, next) => {
+    let token = req.headers.authorization.substr(7);
+    api.users.get(token, (err, res) => {
+      if (err) {
+        return next("route");
+      }
+      req.user = res;
+      next();
+    });
+  });
   /* GET users listing. */
   router.get("/", function(req, res, next) {
     res.send("respond with a smile");
   });
 
-  // curl -X GET  http://localhost:3002/1
+  // curl -X GET  http://localhost:3002/api/users/1
   router.get("/:id", function(req, res) {
     api.users.get(req.params.id, (err, user) => {
+      if (err) {
+        res.status(403).send(err.message);
+      }
       res.send(user);
     });
   });
@@ -28,7 +41,7 @@ module.exports = api => {
      curl -X PUT http://localhost:3002/1 -H "Content-Type: application/json" -d "{\"email\":\"fiona@freshfridge.com\"}"
    */
   router.put("/:id", function(req, res) {
-    api.users.set(req.params.id, req.body, (err, user) => {
+    api.users.set(req.user.id, req.body, (err, user) => {
       if (err) {
         res.status(400).send(err.message);
       }
