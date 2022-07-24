aa8f92727abb6b8ddc82759bdcbb761d071dbb8e
FionaLin <fiona.cy.lin@gmail.com>
2022-07-24 23:11:11 +1000

    Fix routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,41 @@
-import React from "react";
-import { connect } from "react-redux";
-import PropsRoute from "./constants/routeConst";
-import { Route, BrowserRouter as Router } from "react-router-dom";
-import Login from "./pages/login/container.js";
-import Signup from "./pages/Signup";
-import Dashboard from "./pages/dashboard/container.js";
-import Profile from "./pages/profile/container.js";
-import MealList from "./pages/meal-list/container.js";
-import ProfileEdit from "./pages/ProfileEdit";
-import NavigationBar from "./pages/navigation-bar/container.js";
-import "./App.css";
-import RecipePage from "./pages/recipe/container.js";
-import RecipeList from "./pages/recipe-list/container.js";
-import BookMarks from "./pages/book-mark/container.js";
-import EditRecipe from "./pages/edit-recipe/container.js";
-import PropTypes from "prop-types";
+import React from 'react';
+import {connect} from 'react-redux';
+import PropsRoute from './constants/routeConst';
+import {Route, Routes, BrowserRouter as Router} from 'react-router-dom';
+import Login from './pages/login/container.js';
+import Signup from './pages/Signup';
+import Dashboard from './pages/dashboard/container.js';
+import Profile from './pages/profile/container.js';
+import MealList from './pages/meal-list/container.js';
+import ProfileEdit from './pages/ProfileEdit';
+import NavigationBar from './pages/navigation-bar/container.js';
+import './App.css';
+import RecipePage from './pages/recipe/container.js';
+import RecipeList from './pages/recipe-list/container.js';
+import BookMarks from './pages/book-mark/container.js';
+import EditRecipe from './pages/edit-recipe/container.js';
+import PropTypes from 'prop-types';
 
-const App = ({ store }) => {
+const App = ({store}) => {
   return (
     <Router>
       <div store={store}>
-        <Route path="/" component={NavigationBar} />
-        <Route path="/login" exact strict component={Login} />
-        <Route path="/signup" exact strict component={Signup} />
-        <Route path="/" exact strict component={Login} />
-        <PropsRoute path="/dashboard" exact strict component={Dashboard} />
-        <PropsRoute path="/recipes" exact strict component={RecipeList}/>
-        <PropsRoute path="/recipe/:id" component={RecipePage} />
-        <PropsRoute path="/recipes/:mode/:id" exact strict component={EditRecipe} />
-        <PropsRoute path="/recipes/:mode" exact strict component={EditRecipe} />
+        <Routes>
+          <Route path='/' element={<NavigationBar />} />
+          <Route path='/login' exact strict element={<Login />} />
+          <Route path='/signup' exact strict element={<Signup />} />
+          <Route path='/' exact strict element={<Login />} />
+          <Route path='/dashboard' exact strict element={<Dashboard />} />
+          <Route path='/recipes' exact strict element={<RecipeList />} />
+          <Route path='/recipe/:id' element={<RecipePage />} />
+          <Route path='/recipes/:mode/:id' exact strict element={<EditRecipe />} />
+          <Route path='/recipes/:mode' exact strict element={<EditRecipe />} />
 
-        <PropsRoute path="/meal-list" exact strict component={MealList} />
-        <PropsRoute path="/bookmark" exact strict component={BookMarks} />
+          <Route path='/meal-list' exact strict element={<MealList />} />
+          <Route path='/bookmark' exact strict element={<BookMarks />} />
+          <Route path='/profile' exact strict element={<Profile />} />
+          <Route path='/profile/edit' element={<ProfileEdit />} />
+        </Routes>
 
         {/* 
               <PropsRoute path="/dashboard" exact strict component={Dashboard} recipeInfo={this.state.recipes}/>
@@ -44,8 +48,6 @@ const App = ({ store }) => {
                   users={this.state.users} curr_user={CURR_USER_ID} mealPlans={this.state.mealPlans} recipeInfo={this.state.recipes}
                   editPlan={(mealPlans) => this.setState({mealPlans})}/>
           */}
-        <PropsRoute path="/profile" exact strict component={Profile} />
-        <PropsRoute path="/profile/edit" component={ProfileEdit} />
       </div>
     </Router>
   );
