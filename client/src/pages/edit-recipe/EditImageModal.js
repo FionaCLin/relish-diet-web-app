e01143584b084ecc25db3e15aaa2d75828635338
Joel Dobbins <joeldobbinsdesigns@gmail.com>
2022-07-24 15:01:09 +1000

    make Boostap React modal work, and fix animation bug where the modal displays but as invisible

--- client/src/pages/edit-recipe/EditImageModal.js
+++ client/src/pages/edit-recipe/EditImageModal.js
@@ -1,6 +1,12 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+
+const styles = {
+    navStyle: { background: "red" },
+    noteStyle: { color: "yellow", fontSize: "20px" },
+    btnStyle: { background: "blue", marginLeft: "10px" }
+  };
 
 function EditImageModal() {
   const [show, setShow] = useState(false);
@@ -9,26 +15,26 @@ function EditImageModal() {
   const handleShow = () => setShow(true);
 
   return (
-    <>
-      <Button variant="primary" onClick={handleShow}>
-        Launch demo modal
+    <div>
+      <Button style={styles.btnStyle} variant='primary' onClick={handleShow}>
+        Take Note
       </Button>
 
-      <Modal show={show} onHide={handleClose}>
+      <Modal show={show} onHide={handleClose} style={{opacity:1}}>
         <Modal.Header closeButton>
           <Modal.Title>Modal heading</Modal.Title>
         </Modal.Header>
         <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
+          <Button variant='secondary' onClick={handleClose}>
             Close
           </Button>
-          <Button variant="primary" onClick={handleClose}>
+          <Button variant='primary' onClick={handleClose}>
             Save Changes
           </Button>
         </Modal.Footer>
       </Modal>
-    </>
+    </div>
   );
 }
 
